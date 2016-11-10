54c86061950c3ccd0cc108d33cd371e0ceaf3f33
Ariel Hernandez <ariel.h.estevenz@gmail.com>
2016-11-10 15:06:42 -0300

    Feature: [apSimpleRegister] Several methods
    Methods for image management and file logging have been added. Futhermore, I made a enum to identify the parameter map templates for image registration. The maps are: translation, rigid, affine and bspline from SimpleElastix and SimpleITK.

--- apSimpleRegister.cpp
+++ apSimpleRegister.cpp
@@ -8,8 +8,18 @@ SimpleRegister::SimpleRegister(string project_path)
   ,movingReader(shared_ptr<sitk::ImageFileReader> (new sitk::ImageFileReader()))
   ,registeredImageWriter(shared_ptr<sitk::ImageFileWriter> (new sitk::ImageFileWriter()))
   ,pathToImages(project_path)
+  ,log(false)
 {
 
+}
+
+SimpleRegister::parameter_map_code SimpleRegister::GetParameterMapTemplateStringValues(string const& in_string)
+{
+    if (in_string == "translation") return translation;
+    if (in_string == "rigid") return rigid;
+    if (in_string == "affine") return affine;
+    if (in_string == "bspline") return bspline;        
+    else return no_value;
 }
 
 void SimpleRegister::GetImages(string fixed, string moving)
@@ -20,22 +30,95 @@ void SimpleRegister::GetImages(string fixed, string moving)
   elastix->SetFixedImage( fixedReader->Execute() );  
 }
 
+void SimpleRegister::SetImageSize( sitk::Image * image)
+{ 
+  //TODO: Complete this method
+}
+
+vector<unsigned int> SimpleRegister::GetImageSize( sitk::Image image)
+{
+  return image.GetSize();
+}
+
 void SimpleRegister::SetParameterMapFromFile(string parameter_map_file)
 {
   elastix->SetParameterMap(sitk::ReadParameterFile( string( parameter_map_file)));
 }
 
-void SimpleRegister::ExecuteRegister()
+void SimpleRegister::SetParameterMapFromTemplate(string parameter_map_template)
 {
+    string map_template;
+    
+    switch(GetParameterMapTemplateStringValues(parameter_map_template)){
+	case translation:
+            map_template="translation";
+            break;
+        case rigid:
+            map_template="rigid";
+            break;        
+	case affine:
+            map_template="affine";
+            break;
+	case bspline:
+            map_template="bspline";
+	    break;
+        default:
+	    std::cerr<<"Template no recognized"<<endl;
+            break;
+    }    
+ 
+    elastix->SetParameterMap(sitk::GetDefaultParameterMap(map_template));
+}
+
+void SimpleRegister::ExecuteRegister()
+{  
   elastix->Execute();
 }
 
 void SimpleRegister::SetRegisteredImage(string registered_image)
 {
   registeredImageWriter->SetFileName( pathToImages+"/"+registered_image );
-  registeredImageWriter->Execute(elastix->GetResultImage());
-  
+  registeredImageWriter->Execute(elastix->GetResultImage());  
 }
+
+void SimpleRegister::ShowImage()
+{
+  //TODO: Complete this method
+}
+
+void SimpleRegister::GetTransformationMatrix()
+{
+  //TODO: Complete this method
+}
+
+void SimpleRegister::SetFilenameLog(string filename)
+{   
+   elastix->SetLogFileName(filename);
+   elastix->SetLogToFile(true);
+   log=true;
+}
+
+void SimpleRegister::LogToFile(bool on)
+{
+  if(log)
+  { if(on)
+      elastix->LogToFileOn();
+    else
+      elastix->LogToFileOff();
+  }
+  else
+    cerr<<"Please set the filename log first"<<endl;
+}
+
+void SimpleRegister::LogToConsole(bool on)
+{
+  if(on)
+    elastix->LogToConsoleOn();
+  else
+    elastix->LogToConsoleOff();
+}
+
+
 SimpleRegister::~SimpleRegister()
 {
 
--- apSimpleRegister.hpp
+++ apSimpleRegister.hpp
@@ -14,27 +14,43 @@ namespace ap {
 
     class SimpleRegister
     {
+    private:
+      enum parameter_map_code {translation, rigid, affine, bspline, no_value};
+
     public:
       SimpleRegister(string project_path);
       void GetImages(string fixed, string moving);
       void SetRegisteredImage(string registered_image);
       void SetParameterMapFromFile(string parameter_map_file);
+      void SetParameterMapFromTemplate(string parameter_map_template);
+      void GetTransformationMatrix();
+      void ShowImage();
+      void SetFilenameLog(string filename);
+      void LogToFile(bool on);
+      void LogToConsole(bool on);
       void ExecuteRegister();
       ~SimpleRegister();
-	
+
+    private:
+      parameter_map_code GetParameterMapTemplateStringValues(string const& in_string);
+      void SetImageSize( sitk::Image * image);
+      vector<unsigned int> GetImageSize( sitk::Image image);
+      bool log;      
+      
     private:
       shared_ptr<sitk::SimpleElastix> elastix;
-      shared_ptr<sitk::ImageFileReader> fixedReader, movingReader;
+      shared_ptr<sitk::ImageFileReader> fixedReader, movingReader;      
       shared_ptr<sitk::ImageFileWriter> registeredImageWriter;
       string pathToImages;
-      
+
     public:
       static const int OK=0000;
       static const int FAILED=0001;
-      
+
     public:
       sitk::Image GetFixedImage() const { return fixedReader->Execute(); }
       sitk::Image GetMovingImage() const { return movingReader->Execute(); }
+      sitk::Image GetRegisteredImage() const { return elastix->GetResultImage(); }
     };
 }
 
