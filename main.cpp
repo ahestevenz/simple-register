afffdb1ab87853a833e1ce7f70500b795e0ad315
Ariel Hernandez <ariel.h.estevenz@gmail.com>
2016-11-10 14:59:53 -0300

    Added: [simple-register] CommandLineArgumentParser
    Commandline parser was added. Incomplete.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "apSimpleRegister.hpp"
+#include "utils/itkCommandLineArgumentParser.h"
 
 using namespace ap;
 
@@ -9,10 +10,14 @@ int main ( int argc, char* argv[] ) {
     std::cerr << "Usage: " << argv[0] << " <path_to_images> <fixed_name> <moving_name> <parameterFile> <output_image>\n";
     return SimpleRegister::OK;
   }
+  
+  // TODO: Complete command line argument parser function
+  itk::CommandLineArgumentParser::Pointer parser = itk::CommandLineArgumentParser::New();
+  parser->SetCommandLineArguments( argc, argv );
 
   shared_ptr<SimpleRegister> imgregister(new SimpleRegister(string(argv[1])));
   imgregister->GetImages(string(argv[2]),string(argv[3]));
-  imgregister->SetParameterMapFromFile(string(argv[4]));
+  imgregister->SetParameterMapFromFile(string(argv[4])); 
   imgregister->ExecuteRegister();  
   imgregister->SetRegisteredImage(string(argv[5]));
 
