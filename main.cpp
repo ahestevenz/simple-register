eb1362634fad6311f32c603ba7a5fcf3c84f2e31
Ariel Hernandez <ariel.h.estevenz@gmail.com>
2016-11-16 10:50:52 -0300

    Added: [simple-register] Commandline parser and show images
    I implemented QCommandLineParser object and I fixed a bug in sitkShow.cxx to show images.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "apSimpleRegister.hpp"
-#include "utils/itkCommandLineArgumentParser.hpp"
 #include <QCommandLineParser>
 
 using namespace ap;
@@ -7,21 +6,85 @@ using namespace ap;
 
 int main ( int argc, char* argv[] ) {
 
-  if ( argc < 4 ) {
-    std::cerr << "Usage: " << argv[0] << " <path_to_images> <fixed_name> <moving_name> <parameterFile> <output_image>\n";
-    return SimpleRegister::OK;
-  }
-  
-  // TODO: Complete command line argument parser function
-  itk::CommandLineArgumentParser::Pointer parser = itk::CommandLineArgumentParser::New();
-  parser->SetCommandLineArguments( argc, argv );
+  QCoreApplication arg(argc, argv);
+    QCoreApplication::setApplicationName(argv[0]);
+    QCoreApplication::setApplicationVersion("0.1");
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
 
-  shared_ptr<SimpleRegister> imgregister(new SimpleRegister(string(argv[1])));
-  imgregister->GetImages(string(argv[2]),string(argv[3]));
-  imgregister->SetParameterMapFromFile(string(argv[4])); 
+    QCommandLineOption cfg(QStringList()<<"i"<<"images","Path to moving and fixed images.","/directory_1/images/");
+    QCommandLineOption par(QStringList()<<"p"<<"parameters","Path to the parameter file configuration.","/directory_1/parameter_1.txt");
+    QCommandLineOption fix(QStringList()<<"f"<<"fixed","Fixed image name.","ct.dmg");
+    QCommandLineOption mov(QStringList()<<"m"<<"moving","Moving image name.","pet.dmg");
+    QCommandLineOption reg(QStringList()<<"r"<<"registered","Registered image name.","pet_reg.dcm");
+    QCommandLineOption shw(QStringList()<<"s"<<"show","Show information on the console.","OK");
+
+    parser.addOption(cfg);
+    parser.addOption(par);
+    parser.addOption(fix);
+    parser.addOption(mov);
+    parser.addOption(reg);
+    parser.addOption(shw);
+    parser.process(arg);
+
+    string pathToImages;
+    string pathToParameterFile;
+    string FixedImageName;
+    string MovingImageName;
+    string RegisteredImageName;    
+    string showCheck;
+    
+    if(arg.arguments().length()<10)
+    {
+        parser.showHelp(SimpleRegister::OK);
+    }
+
+    if(parser.isSet("images"))
+    {
+        pathToImages=parser.value("images").toStdString();
+    }
+
+    if(parser.isSet("parameters"))
+    {
+        pathToParameterFile=parser.value("parameters").toStdString();
+    }
+
+    if(parser.isSet("fixed"))
+    {
+        FixedImageName=parser.value("fixed").toStdString();
+    }
+
+    if(parser.isSet("moving"))
+    {
+        MovingImageName=parser.value("moving").toStdString();
+    }
+    
+    if(parser.isSet("registered"))
+    {
+        RegisteredImageName=parser.value("registered").toStdString();
+    }
+
+    if(parser.isSet("show"))
+    {
+        showCheck=parser.value("show").toStdString();
+    }
+    
+    bool verbose=false;
+    if(strcmp(showCheck.c_str(),"OK")==0) verbose=true;
+    
+  shared_ptr<SimpleRegister> imgregister(new SimpleRegister(pathToImages));
+    
+  imgregister->GetImages(FixedImageName,MovingImageName);
+  imgregister->SetParameterMapFromFile(pathToParameterFile); 
+  if (verbose) imgregister->LogToConsole();
   imgregister->ExecuteRegister();  
-  imgregister->SetRegisteredImage(string(argv[5]));
-
+  imgregister->SetRegisteredImage(RegisteredImageName);
+  
+  imgregister->ShowFixedImage();
+  imgregister->ShowMovingImage();
+  imgregister->ShowRegisteredImage();  
+  
   return SimpleRegister::OK;
 }
 
